5b74b45ba5728a78b1de8f87d8497c5b0f0f4e91
Agony <gcl1197134883@gmail.com>
2025-04-21 20:14:41 +0800

    Initial commit

---
+++ src/main/java/com/agony/alarmsystem/AlarmsystemApplication.java
@@ -0,0 +1,13 @@
+package com.agony.alarmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agony/alarmsystem/AlarmsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agony.alarmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
